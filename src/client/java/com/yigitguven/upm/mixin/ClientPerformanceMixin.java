6cb7c0f51ab695f746990e87fc133f55f085fec2
Yiğit Güven <contact@yigitguven.net>
2026-02-25 10:52:14 +0100

    Refactor client mixins and client initializer
    Make the mod client-only and reorganize mixins and entrypoints. Added a MOD_ID and SLF4J LOGGER to UltimatePerformanceModClient and log initialization. Renamed and moved mixin classes (ExampleClientMixin -> ClientPerformanceMixin, ExampleMixin -> InternalServerMixin) into com.yigitguven.upm.mixin and updated upm.client.mixins.json to reference both mixins. Removed the common ModInitializer (UltimatePerformanceMod) and the global upm.mixins.json, and updated fabric.mod.json metadata (description, author/contact, license, environment set to client) and mixin configuration to the client-only config.

--- src/client/java/com/yigitguven/upm/mixin/ClientPerformanceMixin.java
+++ src/client/java/com/yigitguven/upm/mixin/ClientPerformanceMixin.java
@@ -1,4 +1,4 @@
-package com.yigitguven.upm.mixin.client;
+package com.yigitguven.upm.mixin;
 
 import net.minecraft.client.Minecraft;
 import org.spongepowered.asm.mixin.Mixin;
@@ -7,7 +7,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Minecraft.class)
-public class ExampleClientMixin {
+public class ClientPerformanceMixin {
 	@Inject(at = @At("HEAD"), method = "run")
 	private void init(CallbackInfo info) {
 		// This code is injected into the start of Minecraft.run()V
