27151dab12fd62931e4e7b0fcb3dfd89cf75a4a6
Yiğit Güven <contact@yigitguven.net>
2026-02-25 10:44:15 +0100

    Add Fabric mod project scaffold
    Initial project scaffold for the "Ultimate Performance Mod" 1.21.11 Fabric mod.

---
+++ src/client/java/com/yigitguven/upm/UltimatePerformanceModClient.java
@@ -0,0 +1,10 @@
+package com.yigitguven.upm;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class UltimatePerformanceModClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
